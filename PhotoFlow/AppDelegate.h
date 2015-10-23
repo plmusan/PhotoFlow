11f377f7ae0a8489c2b21207c8f17c157510d9b9
plmusan <xWang@MAC041.local>
2015-10-23 15:12:50 +0800

    Initial Commit

---
+++ PhotoFlow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoFlow
+//
+//  Created by x.wang on 15/10/23.
+//  Copyright (c) 2015年 x.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhotoFlow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoFlow
+//
+//  Created by x.wang on 15/10/23.
+//  Copyright (c) 2015年 x.wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
